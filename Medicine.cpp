7065a35180f7ba6f05998320c1e6a889b1c9bc5c
ukityeamyai <yamyai.8084@gmail.com>
2022-10-20 14:55:52 +0700

    64160174
    medicine cpp

---
+++ Medicine.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Acer on 18/10/2565.
+//
+
+#include "Medicine.h"
+Medicine::Medicine() {
+    this->type = 0;
+    this->name = "";
+    this->price = 0;
+}
+
+void Medicine::set_type(int type) {
+    this->type = type;
+}
+
+void Medicine::set_name(string name) {
+    this->name = name;
+}
+
+void Medicine::set_price(int price) {
+    this->price = price;
+}

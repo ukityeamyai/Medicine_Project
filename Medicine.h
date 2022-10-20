9c9675753198d8b07d32f35a732c2522c4cef60a
ukityeamyai <yamyai.8084@gmail.com>
2022-10-20 14:51:13 +0700

    64160174
    medicine h

---
+++ Medicine.h
@@ -0,0 +1,20 @@
+#ifndef PROJECT_MEDICINE_H
+#define PROJECT_MEDICINE_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Medicine {
+private:
+    int type, price;
+    string name;
+public:
+    Medicine();
+    void set_type(int type);
+    void set_name(string name);
+    void set_price(int price);
+};
+
+
+#endif //PROJECT_MEDICINE_H

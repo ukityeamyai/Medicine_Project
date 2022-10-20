219ef0d88787c38675cf93e62a2783be428fac21
ukityeamyai <yamyai.8084@gmail.com>
2022-10-20 14:58:14 +0700

    64160174
    class node

---
+++ Node.h
@@ -0,0 +1,28 @@
+//
+// Created by Acer on 18/10/2565.
+//
+
+#ifndef PROJECT_NODE_H
+#define PROJECT_NODE_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+    int lot_number, amount, volume, price, type;
+    string name, mfg, exp;
+    Node *link_lot;
+
+    string day_mfg, month_mfg, year_mfg;
+    string day_exp, month_exp, year_exp;
+    string date;
+    Node *link_circulation;
+public:
+    Node(int lot_number, int type, string name, int amount, int volume, string mfg, string exp, int price);
+
+    Node(int type, string name, int amount,int price,string date);
+};
+
+
+#endif //PROJECT_NODE_H

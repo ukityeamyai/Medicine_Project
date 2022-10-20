8d7ad3ed256d99d21c530c3e5990e06ae7bb43e7
64160072 <115350906+64160072@users.noreply.github.com>
2022-10-20 16:07:28 +0700

    Update Report.h

---
+++ Report.h
@@ -0,0 +1,22 @@
+#ifndef PROJECT_REPORT_H
+#define PROJECT_REPORT_H
+#include "Storage.h"
+
+class Report {
+    private:
+        Storage obj_storage;
+    public:
+        void set_storage(Storage storage);
+        string comma(int amount);
+        string modify_type(string type);
+        void modify_month_mfg();
+        void modify_month_exp();
+        string modify_month(string month);
+        void bubble_sort_ascending();
+
+        void print_all_medicine();
+        void print_medicine_by_name();
+
+};
+
+#endif //UNTITLED4_REPORT_H
